80c7bf14259a8ef6cf2cb2e619147951d8eacc06
xsolutionsonline <xsolutionsonline@gmail.com>
2024-01-12 09:33:43 -0500

    curso de golang en platzi

---
+++ src/main.go
@@ -0,0 +1,13 @@
+// package es el nombre de la carpeta en el cual esta guardado
+package main
+
+import "fmt"
+
+// funcion principal que va a ejecutar el codigo al ser invocado
+// func = crear funcion
+
+func main() {
+	// impresino de consola fmt
+	fmt.Println("Hola mundo jerry")
+
+}
