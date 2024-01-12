9cd90cf6a8860d1538c6351f2b49e3eb9027b3c7
Jerry Leon Restrepo Moncada - Ceiba Software <jerry.restrepo@C02FPGULMD6M.local>
2024-01-12 16:52:34 -0500

    curso variables tipos de variables

--- src/main.go
+++ src/main.go
@@ -1,13 +1,80 @@
-// package es el nombre de la carpeta en el cual esta guardado
 package main
 
 import "fmt"
 
-// funcion principal que va a ejecutar el codigo al ser invocado
-// func = crear funcion
-
 func main() {
-	// impresino de consola fmt
-	fmt.Println("Hola mundo jerry")
+	const pi float64 = 3.14
+	const pi2 = 3.15
 
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
+
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+
+	fmt.Println("Area cuadrado: ", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	suma := x + y
+	fmt.Println("suma", suma)
+
+	resta := x - y
+	fmt.Println("resta", resta)
+
+	multipliacion := x * y
+	fmt.Println("multipliacion", multipliacion)
+
+	division := x / y
+	fmt.Println("division", division)
+
+	modulo := x % y
+	fmt.Println("modulo", modulo)
+
+	x++
+	fmt.Println("incremental", x)
+
+	x--
+	fmt.Println("decrementa", x)
+
+	//Paquete fmt
+
+	helloMessage := "hello"
+	worldMessage := "world"
+
+	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
+
+	//printf
+	nombre := "platzi"
+	cursos := 500
+
+	fmt.Printf("%s tiene mas de %d cursos \n", nombre, cursos)
+	fmt.Printf("%v tiene mas de %v cursos \n", nombre, cursos)
+
+	// Sprintf
+	message := fmt.Sprintf("%s tiene mas de %d cursos \n", nombre, cursos)
+	fmt.Println("message: ", message)
+
+	// tipos de datos
+	fmt.Printf("helloMessage: %T \n\n", helloMessage)
+	fmt.Printf("CURSOS: %T \n", cursos)
 }
+
+/*
+
+ */
